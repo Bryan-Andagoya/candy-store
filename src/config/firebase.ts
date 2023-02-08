49c1732c0bbb54ed4d63969553e851458b29f082
Bryan-Andagoya <bandagoya@itsqmet.edu.ec>
2023-02-07 21:25:01 -0500

    image picker done

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,12 +5,13 @@ import {
   getReactNativePersistence,
 } from "firebase/auth/react-native";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyBV6Pf-HxlrlKXMRxS3QkhRbW7PUsMnC-U",
   authDomain: "candy-store-1a6d6.firebaseapp.com",
   projectId: "candy-store-1a6d6",
-  storageBucket: "candy-store-1a6d6.appspot.com",
+  storageBucket: "gs://candy-store-1a6d6.appspot.com",
   messagingSenderId: "883940146392",
   appId: "1:883940146392:web:69cb958ba910a654caabad",
 };
@@ -20,3 +21,5 @@ export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
